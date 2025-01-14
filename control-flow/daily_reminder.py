c6460a8727c07e3a047acecbbc47f00bb1f9cdf2
kazeem jamiu <kazeemj565@gmail.com>
2025-01-14 16:51:39 +0100

    Add daily_reminder file

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,12 +1,12 @@
-task_description = input("Enter your task: ")
-task_priority = input("Priority (high/medium/low): ")
-task_due_date = input("Is it time-bound? (yes/no): ")
-match (task_priority, task_description):
+description = input("Enter your task: ")
+priority = input("Priority (high/medium/low): ")
+due_date = input("Is it time-bound? (yes/no): ")
+match (priority):
     case "high":
-        if task_due_date == "yes":
-            print(f"Reminder: {task_description} is a {task_priority} priority task that requires immediate attention today!")
+        if due_date == "yes":
+            print(f"Reminder: {description} is a {priority} priority task that requires immediate attention today!")
     case "low":
-        if task_due_date == "no":
-            print(f"Note: {task_description} is a {task_priority} priority task. Consider completing it when you have free time.")
+        if due_date == "no":
+            print(f"Note: {description} is a {priority} priority task. Consider completing it when you have free time.")
     case _:
         print("Invalide priority level. Please enter high, medium, or low as the priority.")
