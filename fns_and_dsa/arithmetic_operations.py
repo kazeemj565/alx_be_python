9a4a3292a2095fcdbc66a41a7d1fd8f2bc6f511f
kazeem jamiu <kazeemj565@gmail.com>
2025-01-18 10:55:27 +0100

    Adding arithmetic operation file

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,16 @@
+def perform_operation(num1, num2, operation):
+    if operation == '+':
+        return num1 + num2
+    elif operation == '*':
+        return num1 * num2
+    elif operation == '-':
+        return num1 - num2
+    elif operation == '/':
+        return num1 / num2
+    else:
+        return "Invalid operation"
+    
+
+
+# perform_operation(num1, num2, operation(add, subtract, multiply, divide))
+    
