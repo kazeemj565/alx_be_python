3b78f483e92d6a88f83377bef1b49c1d07d674b3
kazeem jamiu <kazeemj565@gmail.com>
2025-01-18 15:41:10 +0100

    Adding temperature convertion file

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,27 @@
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+degree_sign = u"\u00B0"
+
+def convert_to_celsius(fahrenheit):
+    temp_celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+    return temp_celsius
+
+
+def convert_to_fahrenheit(celsius):
+    temp_fahrenheit = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+    return temp_fahrenheit
+    
+
+
+temperature = float(input("Enter the temperature to convert: "))
+unit = input("is this temperature in Celsius or Fahrrenheit? (C/F): ")
+if unit == "F":
+    new_temp = convert_to_celsius(temperature)
+    print(f"{temperature}{degree_sign}F is {new_temp} C")
+
+elif unit == "C":
+    new_temp = convert_to_fahrenheit(temperature)
+    print(f"{temperature}{degree_sign}C is {new_temp} F")
+
+else:
+    print("Invalid unit. Please enter C or F.")
