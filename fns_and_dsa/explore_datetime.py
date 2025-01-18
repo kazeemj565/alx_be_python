d08766995476e4eb75ab5b37f63205a98d20ecf6
kazeem jamiu <kazeemj565@gmail.com>
2025-01-18 14:46:28 +0100

    Adding exploring datetime file

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,25 @@
+from datetime import datetime, timedelta
+from datetime import date
+
+# local_datetime = datetime.now()
+# print(local_datetime)
+
+
+# two_hurricanes_dates = [date(2020, 8, 27), date(2020, 9, 14)]
+# print(two_hurricanes_dates)
+
+
+def display_current_datetime():
+    current_date = datetime.now()
+    current_date = current_date.strftime("%Y-%M-%D %H:%m:%S")
+    print(current_date)
+
+
+display_current_datetime()
+
+def calculate_future_date():
+    future_date = datetime.now() + timedelta(days=40)
+    future_date = future_date.strftime("%Y-%M-%D")
+
+
+calculate_future_date()
