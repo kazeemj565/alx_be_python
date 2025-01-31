73cb4c2f9b861e32d8ae40e8b099a036e7f1e385
kazeem jamiu <kazeemj565@gmail.com>
2025-01-31 21:07:27 +0100

    Adding bank account file

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -9,12 +9,16 @@ class BankAccount:
 
     def withdraw(self, amount):
         # self.account_balance -= amount
-        if amount > self.account_balance:
-            return self.account_balance
-        else:
+        if amount > 0 and amount <= self.account_balance:
             self.account_balance -= amount
-            # print(f"Withdrew: ${amount:.2f}")
-        pass
+        else:
+            return  None 
+        # if amount > self.account_balance:
+        #     return self.account_balance
+        # else:
+        #     self.account_balance -= amount
+        #     # print(f"Withdrew: ${amount:.2f}")
+        # pass
 
     def display_balance(self):
         amount = float(self.account_balance)
