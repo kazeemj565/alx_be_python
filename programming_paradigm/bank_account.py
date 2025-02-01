b69963d9e4f1eb51a44bd4369b4451cc472bc3b3
kazeem jamiu <kazeemj565@gmail.com>
2025-02-01 12:27:31 +0100

    Adding robust division calculator file

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -5,24 +5,17 @@ class BankAccount:
 
     def deposit(self, amount):
         self.account_balance += amount
-        pass
+    
 
     def withdraw(self, amount):
-        # self.account_balance -= amount
         if amount <= self.account_balance:
             self.account_balance -= amount
             return self.account_balance
         else:
             return  None 
-        # if amount > self.account_balance:
-        #     return self.account_balance
-        # else:
-        #     self.account_balance -= amount
-        #     # print(f"Withdrew: ${amount:.2f}")
-        # pass
-
+        
     def display_balance(self):
         amount = float(self.account_balance)
         print(f"Current Balance: ${amount:.2f}")
-        # self.account_balance = 
+         
 
---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,18 @@
+def safe_divide(numerator, denominator):
+    numerator = int(numerator)
+    denominator = int(denominator)
+
+
+    try:
+        # if denominator != 0:
+        result = numerator / denominator
+        print("The result of the division is", result)
+    except ZeroDivisionError:
+        print("Error: Cannot divide by zero")
+    except ValueError:
+        print("Error: Please enter numeric values only.")   
+
+
+    
+    # else:
+    #     print("Invalid Operation")
