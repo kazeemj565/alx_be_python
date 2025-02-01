a8b2958c3858bbe67170f964f12ed003d8aad590
kazeem jamiu <kazeemj565@gmail.com>
2025-02-01 16:33:39 +0100

    Adding test_simple_calculator file

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -12,17 +12,17 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(-4, 3), -1)
 
 
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(4, 2), 2.0)
         self.assertRaises(ZeroDivisionError, self.calc.divide(4, 0))
 
 
 
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(4, 3), 1)
 
 
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(4, 3), 12)
         self.assertEqual(self.calc.multiply(-4, -3), 12)
 
