17d3de945772fd7b2275819450607cd039c322f5
kazeem jamiu <kazeemj565@gmail.com>
2025-02-01 16:30:40 +0100

    Adding test_simple_calculator file

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -2,31 +2,29 @@ import unittest
 from simple_calculator import SimpleCalculator
 
 class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        """Set up the SimpleCalculator instance before each test."""
+        self.calc = SimpleCalculator()
     
     def test_add(self):
-        calculator = SimpleCalculator()
-        result = calculator.add(4, 3)
-        self.assertEqual(result, 7)
+        self.assertEqual(self.calc.add(4, 3), 7)
+        self.assertEqual(self.calc.add(-4, 3), -1)
 
 
     def test_divide(self):
-        calculator = SimpleCalculator()
-        result = calculator.divide(4, 2)
-        self.assertEqual(result, 2.0)
-
+        self.assertEqual(self.calc.divide(4, 2), 2.0)
+        self.assertRaises(ZeroDivisionError, self.calc.divide(4, 0))
 
 
 
     def test_subtract(self):
-        calculator = SimpleCalculator()
-        result = calculator.subtract(4, 3)
-        self.assertEqual(result, 1)
+        self.assertEqual(self.calc.subtract(4, 3), 1)
 
 
     def test_multiply(self):
-        calculator = SimpleCalculator()
-        result = calculator.multiply(4, 3)
-        self.assertEqual(result, 12)
+        self.assertEqual(self.calc.multiply(4, 3), 12)
+        self.assertEqual(self.calc.multiply(-4, -3), 12)
 
 
 if __name__ == "__main__":
