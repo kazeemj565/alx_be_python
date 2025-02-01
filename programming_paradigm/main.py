26b0ce929109324a96c05ff90b7668fd072975f2
kazeem jamiu <kazeemj565@gmail.com>
2025-02-01 15:09:26 +0100

    Adding robust divide calculator file

--- programming_paradigm/main.py
+++ programming_paradigm/main.py
@@ -1,21 +1,3 @@
-# import sys
-# from robust_division_calculator import safe_divide
-
-# def main():
-#     if len(sys.argv) != 3:
-#         print("Usage: python main.py <numerator> <denominator>")
-#         sys.exit(1)
-
-#     numerator = sys.argv[1]
-#     denominator = sys.argv[2]
-
-#     result = safe_divide(numerator, denominator)
-#     print(result)
-
-# if __name__ == "__main__":
-#     main()
-
-
 import sys
 from robust_division_calculator import safe_divide
 
@@ -27,7 +9,39 @@ def main():
     numerator = sys.argv[1]
     denominator = sys.argv[2]
 
-    safe_divide(numerator, denominator)
+    result = safe_divide(numerator, denominator)
+    print(result)
 
 if __name__ == "__main__":
     main()
+
+
+# import sys
+# from robust_division_calculator import safe_divide
+
+# def main():
+#     if len(sys.argv) != 3:
+#         print("Usage: python main.py <numerator> <denominator>")
+#         sys.exit(1)
+
+#     numerator = sys.argv[1]
+#     denominator = sys.argv[2]
+
+#     safe_divide(numerator, denominator)
+
+# if __name__ == "__main__":
+#     main()
+
+
+     # try:
+    #     result = [float(numerator) / float(denominator)]
+    #     print("The result of the division is", result)
+        
+    # except (ZeroDivisionError, ValueError) as error:
+    #     match error:
+    #         case ZeroDivisionError():
+    #             print("Error: Cannot divide by zero.")
+
+    #         case ValueError():
+    #             print("Error: Please enter numeric values only.")
+
--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,17 +1,4 @@
 def safe_divide(numerator, denominator):
-    # try:
-    #     result = [float(numerator) / float(denominator)]
-    #     print("The result of the division is", result)
-        
-    # except (ZeroDivisionError, ValueError) as error:
-    #     match error:
-    #         case ZeroDivisionError():
-    #             print("Error: Cannot divide by zero.")
-
-    #         case ValueError():
-    #             print("Error: Please enter numeric values only.")
-
-
     try:
         result = float(numerator) / float(denominator)
         print("The result of the division is", result)
