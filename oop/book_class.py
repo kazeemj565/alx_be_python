abde7bcdd1c6cef6ad719f8d3f7cf7ff50ebd0a2
kazeem jamiu <kazeemj565@gmail.com>
2025-02-10 12:53:56 +0100

    Adding book_class file and the main file

--- oop/book_class.py
+++ oop/book_class.py
@@ -8,7 +8,7 @@ class Book:
     def __str__(self):
         return f"{self.title} by {self.author}, published in {self.year}"
     
-    def __del__(my_book):
+    def __del__(self):
         print("Deleting ")
 
 
