f046b380fc8d27eb3bf3117938b9ab3a288cb151
kazeem jamiu <kazeemj565@gmail.com>
2025-02-10 22:49:24 +0100

    Adding library_system file

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,6 +3,8 @@ class Book:
         self.title = title
         self.author = author
 
+    def __str__(self):
+         pass
 
 class EBook(Book):
         def __init__(self, title, author, file_size):
@@ -16,3 +18,80 @@ class PrintBook(Book):
           self.page_count = page_count
 
           
+# class Car:
+#   def __init__(self, engine):
+#     self.engine = engine  # Engine object as an attribute
+
+#   def start(self):
+#     self.engine.start()
+
+# class Engine:
+#   def start(self):
+#     print("Engine starting...")
+
+# car = Car(Engine())
+# car.start()  # Output: Engine starting...
+
+
+
+# class Duck:  
+#    def swim(self):  
+#          print("I'm a duck, and I can swim.")  
+   
+# class Sparrow:  
+#      def swim(self):  
+#          print("I'm a sparrow, and I can swim.")  
+   
+# class Crocodile:  
+#      def swim_walk(self):  
+#          print("I'm a Crocodile, and I can swim, but not quack.")  
+   
+# def duck_testing(animal):  
+#      animal.swim()  
+       
+       
+# duck_testing(Duck())  
+# duck_testing(Sparrow())  
+# duck_testing(Crocodile())  
+
+
+
+
+
+
+# class Component: 
+  
+#    # composite class constructor 
+#     def __init__(self): 
+#         print('Component class object created...') 
+  
+#     # composite class instance method 
+#     def m1(self): 
+#         print('Component class m1() method executed...') 
+  
+  
+# class Composite: 
+  
+#     # composite class constructor 
+#     def __init__(self): 
+  
+#         # creating object of component class 
+#         self.obj1 = Component() 
+          
+#         print('Composite class object also created...') 
+  
+#      # composite class instance method 
+#     def m2(self): 
+        
+#         print('Composite class m2() method executed...') 
+  
+#         # calling m1() method of component class 
+#         self.obj1.m1() 
+  
+  
+# # creating object of composite class 
+# obj2 = Composite() 
+  
+# # calling m2() method of composite class 
+# obj2.m2() 
+# Output
