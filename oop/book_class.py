cf28a70843b1b2f13c33782f1590a625100cc9d5
kazeem jamiu <kazeemj565@gmail.com>
2025-02-11 06:54:15 +0100

    Adding library_system file

--- oop/book_class.py
+++ oop/book_class.py
@@ -9,10 +9,11 @@ class Book:
         return f"{self.title} by {self.author}, published in {self.year}"
     
     def __del__(self):
-        print("Deleting", self.year)
+        print("Deleting", self.title)
 
 
     def __repr__(self):
         return f"{self.__class__.__qualname__}('{self.title}', '{self.author}', {self.year})"
+    
     
     
